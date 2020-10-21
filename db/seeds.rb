e3237406d663600d715e4c675c4e851e9eedb656
Bapsk <baptiste.kief@gmail.com>
2020-10-21 14:12:27 -0400

    Add user and message-to-db feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Moi", password: "password")
+User.create(username: "Toi", password: "password")
+User.create(username: "Nous", password: "password")
+User.create(username: "Vous", password: "password")
+User.create(username: "Eux", password: "password")
